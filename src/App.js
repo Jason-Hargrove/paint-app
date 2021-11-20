cf4731f9c1f1c476ee0b63c5bbd9acda15ed54cc
Jason Hargrove <rijaha@gmail.com>
2021-11-20 11:01:51 -0600

    Working on landing page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Groovy! Updating for Heroku.</h1>
+      <nav className="navbar">
+        <div className="container">
+          {/* path for image src="/img/logo192.png" */}
+          <img src="/img/mandala-effect-logo.png" alt="logo"/>
+          <h1 className="title">Mandala Effect</h1>
+          <ul className="nav">
+            <li><a href="#home"></a>HOME</li>
+            <li><a href="#about"></a>ABOUT</li>
+            <li><a href="#about"></a>GITHUB</li>
+          </ul>
+        </div>
+      </nav>
+      <h1>Unwind With Color</h1>
+      <h2>A Digital Drawing Experience</h2>
+      <button>Go To Project</button>
+      <img src="" alt=""/>
+
+      <h1>The Challenge</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam modi voluptas cum eum doloremque distinctio, non quia eaque cupiditate itaque incidunt soluta repellendus quod perferendis mollitia error tempora dignissimos. Cum.</p>
+
+      <h1>The Process</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolorem, maxime ipsam quas, alias labore, nulla eius quibusdam ratione voluptas, ab voluptatibus suscipit? Id, ipsa amet. Fugiat facere, similique commodi.</p>
+
+      
+
     </div>
   );
 }
